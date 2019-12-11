cb11feaedca0a79186a6aa7bc63bdc773c86c38e
chinwat kotchum <chinwat@odd.works>
2019-12-11 14:16:21 +0700

    clean up

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -7,6 +7,8 @@ describe('HelloWorld.vue', () => {
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
     });
-    expect(wrapper.text()).toMatch(msg);
+    
+    expect(wrapper.text()).toMatch(`Hi, ${msg}`);
   });
 });
+
